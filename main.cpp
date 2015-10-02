ef1017d686680cdffd09af2c098cbde1280cb03a
nathan <nathan1hughes@gmail.com>
2015-10-02 10:14:32 +0100

    Small changes to comments

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
-
+/*
+ *
+ * Testing documents for SDL2 Engine
+ * 2/10/2015
+ *
+ * Author: Nathan Hughes /w help from lazyfoo
+ * 
+ *
+ */
 
 //Using SDL and standard IO
 #include <SDL.h>
@@ -23,7 +31,9 @@ int main( int argc, char* args[] )
     else
     {
         //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+         SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
         if( window == NULL )
         {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
