67f33d8f6a38cce61845835fe8ef3d6fe7397e8f
Nathan <nathan1hughes@gmail.com>
2015-12-30 20:45:35 +0000

    Updated input library

--- source/headers/input.h
+++ source/headers/input.h
@@ -5,4 +5,30 @@
 #ifndef SDL2_INPUT_H
 #define SDL2_INPUT_H
 
+#include <map>
+#include <SDL2/SDL.h>
+
+class Input{
+
+public:
+    void beginNewFrame();
+    void keyUpEvent(const SDL_Event& event);
+    void keyDownEvent(const SDL_Event& event);
+
+    bool wasKeyPressed(SDL_Scancode key);
+    bool wasKeyReleased(SDL_Scancode key);
+    bool isKeyHeld(SDL_Scancode key);
+
+private:
+
+    std::map<SDL_Scancode, bool> _heldKeys;
+    std::map<SDL_Scancode, bool> _pressedKeys;
+    std::map<SDL_Scancode, bool> _releasedKeys;
+
+
+};
+
+
+
+
 #endif //SDL2_INPUT_H
--- source/src/game.cpp
+++ source/src/game.cpp
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include "../headers/game.h"
 #include "../headers/graphics.h"
+#include "../headers/input.h"
 
 /**
  * Game class
@@ -24,14 +25,34 @@ Game::~Game(){
 void Game::gameLoop(){
 
     Graphics graphics;
+    Input input;
 
     //Event that has happened for this particular frame
     SDL_Event event;
 
     while(true){
+
+        //Begin a new frame at the start of each loop
+        input.beginNewFrame();
+
         if(SDL_PollEvent(&event)){
-            if(event.type == SDL_QUIT)
+
+            //A key was pressed :D
+            if(event.type == SDL_KEYDOWN){
+                //Checks that a key is not a repeated press
+                if(event.key.repeat == 0){
+                    input.keyDownEvent(event);
+                }
+            }// if a key was released
+            else if (event.type == SDL_KEYUP){
+                input.keyUpEvent(event);
+            }//if the user hits the exit button
+            else if (event.type == SDL_QUIT){
                 return;
+            }
+        }
+        if(input.wasKeyPressed(SDL_SCANCODE_ESCAPE) == true){
+            return; 
         }
     }
 
